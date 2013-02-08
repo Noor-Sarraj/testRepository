a461653157350f65f1e9d6e89bcb7bfff3be13ac
Noor Sarraj <noor.sarraj@syrisoft.com>
2013-02-08 17:00:04 +0200

    The first testing commit

---
+++ testGitHub/testGitHub/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  testGitHub
+//
+//  Created by Syrisoft on 2/8/13.
+//  Copyright (c) 2013 Syrisoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, Git Hub!");
+        
+    }
+    return 0;
+}
+
